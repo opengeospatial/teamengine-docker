936a49f52b7d5e54bac604c1114279f8dcc59b82
Dirk Stenger <stenger@lat-lon.de>
2021-12-21 20:17:15 +0100

    Create script build-test-suites-for-beta.sh

---
+++ scripts/build-test-suites-for-beta.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+mkdir tmp-test-suites-beta
+cd tmp-test-suites-beta/
+
+echo Clone test suites
+git clone --depth 1 --branch 1.13 https://github.com/opengeospatial/ets-wfs10.git
+git clone --depth 1 --branch 1.32 https://github.com/opengeospatial/ets-wfs11.git
+git clone --depth 1 --branch 1.18 https://github.com/opengeospatial/ets-csw202.git
+git clone --depth 1 --branch 0.5 https://github.com/opengeospatial/ets-kml2.git
+git clone --depth 1 --branch 1.8 https://github.com/opengeospatial/ets-sfs11.git
+git clone --depth 1 --branch 1.4 https://github.com/opengeospatial/ets-sfs12.git
+git clone --depth 1 --branch 1.14 https://github.com/opengeospatial/ets-sos10.git
+git clone --depth 1 --branch 1.8 https://github.com/opengeospatial/ets-sps10.git
+git clone --depth 1 --branch 1.11 https://github.com/opengeospatial/ets-sps20.git
+
+echo Build test suites
+for d in */ ; do
+    cd "$d" && mvn clean install -DskipTests && cd ..
+done
+
+echo Build embedded-dependencies-repacked of ets-security-client10
+git clone https://github.com/opengeospatial/ets-security-client10.git
+cd ets-security-client10/embedded-dependencies-repacked/
+mvn clean install -DskipTests
+cd ../..
+
+cd ..
+rm -rf tmp-test-suites-beta/
