065961d3ac007adc30d2b75c0117a60b065cff89
Dirk Stenger <stenger@lat-lon.de>
2022-08-31 19:22:06 +0200

    Update Beta test suites ets-wfs20-dgiwg, ets-wms13-nsg and ets-wfs20-nsg

--- scripts/build-test-suites-not-available-via-central-maven-repo.sh
+++ scripts/build-test-suites-not-available-via-central-maven-repo.sh
@@ -2,6 +2,20 @@
 mkdir tmp-teamengine-docker-test-suites
 cd tmp-teamengine-docker-test-suites/
 
+echo Build teamengine 5.0
+git clone --depth 1 --branch 5.0 https://github.com/opengeospatial/teamengine.git
+cd teamengine/
+mvn clean install -DskipTests
+cd ../
+rm -rf teamengine
+
+echo Build ets-dgiwg-core 0.4
+git clone --depth 1 --branch 0.4 https://github.com/opengeospatial/ets-dgiwg-core.git
+cd ets-dgiwg-core/
+mvn clean install -DskipTests
+cd ../
+rm -rf ets-dgiwg-core
+
 echo Build ets-dgiwg-core 0.5
 git clone --depth 1 --branch 0.5 https://github.com/opengeospatial/ets-dgiwg-core.git
 cd ets-dgiwg-core/
@@ -18,7 +32,7 @@ git clone --depth 1 --branch 1.4 https://github.com/opengeospatial/ets-sfs12.git
 git clone --depth 1 --branch 1.14 https://github.com/opengeospatial/ets-sos10.git
 git clone --depth 1 --branch 1.8 https://github.com/opengeospatial/ets-sps10.git
 git clone --depth 1 --branch 1.11 https://github.com/opengeospatial/ets-sps20.git
-git clone --depth 1 --branch 0.5 https://github.com/opengeospatial/ets-wfs20-dgiwg.git
+git clone --depth 1 --branch 0.4 https://github.com/opengeospatial/ets-wms13-dgiwg.git
 git clone --depth 1 --branch 0.5 https://github.com/opengeospatial/ets-wms13-dgiwg.git
 
 echo Build test suites
